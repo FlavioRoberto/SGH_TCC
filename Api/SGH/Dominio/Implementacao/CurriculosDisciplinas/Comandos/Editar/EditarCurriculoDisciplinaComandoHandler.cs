78055eec8027f3ea5e249ee2bb8616dc946f83cd
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2020-03-08 19:10:38 -0300

    Refatoração - Em andamento

--- Api/SGH/Dominio/Implementacao/CurriculosDisciplinas/Comandos/Editar/EditarCurriculoDisciplinaComandoHandler.cs
+++ Api/SGH/Dominio/Implementacao/CurriculosDisciplinas/Comandos/Editar/EditarCurriculoDisciplinaComandoHandler.cs
@@ -1,8 +1,16 @@
 ﻿using MediatR;
+using SGH.Dominio.Core;
+using SGH.Dominio.ViewModel;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SGH.Dominio.Services.Implementacao.CurriculosDisciplinas.Comandos.Editar
 {
-    public class EditarCurriculoDisciplinaComandoHandler : IRequestHandler<>
+    public class EditarCurriculoDisciplinaComandoHandler : IRequestHandler<EditarCurriculoDisciplinaComando, Resposta<CurriculoDisciplinaViewModel>>
     {
+        public Task<Resposta<CurriculoDisciplinaViewModel>> Handle(EditarCurriculoDisciplinaComando request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
---
+++ Api/SGH/Dominio/Implementacao/CurriculosDisciplinas/Comandos/Editar/EditarCurriculoDisciplinaComandoValidador.cs
@@ -0,0 +1,12 @@
+﻿using SGH.Data.Repositorio.Contratos;
+using SGH.Dominio.Services.Implementacao.CurriculosDisciplinas.Comandos.Base;
+
+namespace SGH.Dominio.Services.Implementacao.CurriculosDisciplinas.Comandos.Editar
+{
+    public class EditarCurriculoDisciplinaComandoValidador : CurriculoDisciplinaComandoBaseValidador<EditarCurriculoDisciplinaComando>, IEditarCurriculoDisciplinaComandoValidador
+    {
+        public EditarCurriculoDisciplinaComandoValidador(ICurriculoDisciplinaRepositorio curriculoDisciplinaRepositorio, IDisciplinaRepositorio disciplinaRepositorio) : base(curriculoDisciplinaRepositorio, disciplinaRepositorio)
+        {
+        }
+    }
+}
