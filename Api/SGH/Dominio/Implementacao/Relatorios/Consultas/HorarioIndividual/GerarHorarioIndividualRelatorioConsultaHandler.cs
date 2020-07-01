57ecb945757ff6af36ac9f84c5ffad061d794fc5
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2020-07-01 16:43:34 -0300

    Ajuste na geração dos horários individuais

--- Api/SGH/Dominio/Implementacao/Relatorios/Consultas/HorarioIndividual/GerarHorarioIndividualRelatorioConsultaHandler.cs
+++ Api/SGH/Dominio/Implementacao/Relatorios/Consultas/HorarioIndividual/GerarHorarioIndividualRelatorioConsultaHandler.cs
@@ -23,7 +23,6 @@ namespace SGH.Dominio.Services.Implementacao.Relatorios.Consultas.HorarioIndivid
         private readonly ICurriculoDisciplinaRepositorio _curriculoDisciplinaRepositorio;
         private readonly ICurriculoRepositorio _curriculoRepositorio;
         private readonly IAulaRepositorio _aulaRepositorio;
-        private readonly List<HorarioIndividualAulasData> _aulasRelatorio;
 
         public GerarHorarioIndividualRelatorioConsultaHandler(IRelatorioServico relatorioServico,
                                                               IProfessorRepositorio professorRepositorio,
@@ -40,7 +39,6 @@ namespace SGH.Dominio.Services.Implementacao.Relatorios.Consultas.HorarioIndivid
             _curriculoDisciplinaRepositorio = curriculoDisciplinaRepositorio;
             _curriculoRepositorio = curriculoRepositorio;
             _aulaRepositorio = aulaRepositorio;
-            _aulasRelatorio = new List<HorarioIndividualAulasData>();
         }
 
         public async Task<Resposta<string>> Handle(GerarHorarioIndividualRelatorioConsulta request, CancellationToken cancellationToken)
@@ -64,25 +62,25 @@ namespace SGH.Dominio.Services.Implementacao.Relatorios.Consultas.HorarioIndivid
 
             if (cargos == null || cargos.Count <= 0)
                 return new HorarioIndividualRelatorioData();
-            
+
+            var cargosId = cargos.Select(lnq => lnq.Codigo);
+
+            var disciplinasCargo = await _cargoDisciplinaRepositorio.Listar(lnq => cargosId.Contains(lnq.CodigoCargo));
+
             return new HorarioIndividualRelatorioData
             {
                 Ano = request.Ano,
                 Semestre = request.Semestre.RetornarDescricao(),
                 Cargo = cargos.Select(lnq => $"Cargo: {lnq.Numero} - Edital: {lnq.Edital}").Join(", "),
                 Professor = professor.Nome,
-                DisciplinasMinistradas = await ListarDisciplinasMinistradas(cargos),
-                Aulas = _aulasRelatorio
+                DisciplinasMinistradas = await ListarDisciplinasMinistradas(disciplinasCargo),
+                Aulas = await CarregarAulas(disciplinasCargo)
             };
         }
 
-        private async Task<IList<HorarioIndividualDisciplinaData>> ListarDisciplinasMinistradas(List<Cargo> cargos)
+        private async Task<IList<HorarioIndividualDisciplinaData>> ListarDisciplinasMinistradas(List<CargoDisciplina> disciplinasCargo)
         {
             var disciplinasHorario = new List<HorarioIndividualDisciplinaData>();
-
-            var cargosId = cargos.Select(lnq => lnq.Codigo);
-
-            var disciplinasCargo = await _cargoDisciplinaRepositorio.Listar(lnq => cargosId.Contains(lnq.CodigoCargo));
 
             if (disciplinasCargo == null || disciplinasCargo.Count <= 0)
                 return new List<HorarioIndividualDisciplinaData>();
@@ -94,8 +92,6 @@ namespace SGH.Dominio.Services.Implementacao.Relatorios.Consultas.HorarioIndivid
                 var disciplina = await ConsultarDisciplinaCurriculo(disciplinaCargo.CodigoCurriculoDisciplina);
 
                 var curso = await _curriculoRepositorio.ConsultarCurso(disciplinaCurriculo.CodigoCurriculo);
-
-                await CarregarAula(disciplinaCargo, disciplinaCurriculo, disciplina);
 
                 disciplinasHorario.Add(new HorarioIndividualDisciplinaData
                 {
@@ -112,32 +108,59 @@ namespace SGH.Dominio.Services.Implementacao.Relatorios.Consultas.HorarioIndivid
             
         }
 
-        private async Task CarregarAula(CargoDisciplina disciplinaCargo, CurriculoDisciplina disciplinaCurriculo, Disciplina disciplina)
+        private async Task<List<HorarioIndividualAulasData>> CarregarAulas(List<CargoDisciplina> disciplinasCargos)
         {
-            var aulas = await _aulaRepositorio.Listar(lnq => lnq.CodigoDisciplina == disciplinaCargo.Codigo);
+            var aulasRelatorio = new List<HorarioIndividualAulasData>();
 
-            foreach (var aula in aulas)
+            var codigosDisciplinas = disciplinasCargos.Select(lnq => lnq.Codigo);
+
+            var aulas = await _aulaRepositorio.Listar(lnq => codigosDisciplinas.Contains(lnq.CodigoDisciplina));
+
+            var aulasPorHorario = aulas.GroupBy(lnq => lnq.Reserva.Hora).Select(lnq => lnq);
+
+            foreach(var aulasHorarios in aulasPorHorario)
             {
-                _aulasRelatorio.Add(new HorarioIndividualAulasData
+                var aulaIndividual = new HorarioIndividualAulasData();
+                foreach (var aula in aulasHorarios)
                 {
-                    Hora = aula.Reserva.Hora,
-                    Turno = disciplinaCargo.Turno.Descricao,
-                    DisciplinaSegunda =  RetornarDisciplinaDia("Segunda", aula, (int)disciplinaCurriculo.Periodo, disciplina.Descricao),
-                    DisciplinaTerca =  RetornarDisciplinaDia("Terça", aula, (int)disciplinaCurriculo.Periodo, disciplina.Descricao),
-                    DisciplinaQuarta =  RetornarDisciplinaDia("Quarta", aula, (int)disciplinaCurriculo.Periodo, disciplina.Descricao),
-                    DisciplinaQuinta =  RetornarDisciplinaDia("Quinta", aula, (int)disciplinaCurriculo.Periodo, disciplina.Descricao),
-                    DisciplinaSexta =  RetornarDisciplinaDia("Sexta", aula, (int)disciplinaCurriculo.Periodo, disciplina.Descricao),
-                    DisciplinaSabado =  RetornarDisciplinaDia("Sábado", aula, (int)disciplinaCurriculo.Periodo, disciplina.Descricao)
-                });
+                    var disciplinaCargo = disciplinasCargos.FirstOrDefault(lnq => lnq.Codigo == aula.CodigoDisciplina);
+
+                    aulaIndividual.Hora = aula.Reserva.Hora;
+                    aulaIndividual.Turno = disciplinaCargo.Turno.Descricao;
+
+                    if (string.IsNullOrEmpty(aulaIndividual.DisciplinaSegunda))
+                        aulaIndividual.DisciplinaSegunda = await RetornarDisciplinaDia("Segunda", aula, disciplinaCargo);
+
+                    if (string.IsNullOrEmpty(aulaIndividual.DisciplinaTerca))
+                        aulaIndividual.DisciplinaTerca = await RetornarDisciplinaDia("Terça", aula, disciplinaCargo);
+
+                    if (string.IsNullOrEmpty(aulaIndividual.DisciplinaQuarta))
+                        aulaIndividual.DisciplinaQuarta = await RetornarDisciplinaDia("Quarta", aula, disciplinaCargo);
+
+                    if (string.IsNullOrEmpty(aulaIndividual.DisciplinaQuinta))
+                        aulaIndividual.DisciplinaQuinta = await RetornarDisciplinaDia("Quinta", aula, disciplinaCargo);
+
+                    if (string.IsNullOrEmpty(aulaIndividual.DisciplinaSexta))
+                        aulaIndividual.DisciplinaSexta = await RetornarDisciplinaDia("Sexta", aula, disciplinaCargo);
+
+                    if (string.IsNullOrEmpty(aulaIndividual.DisciplinaSabado))
+                        aulaIndividual.DisciplinaSabado = await RetornarDisciplinaDia("Sábado", aula, disciplinaCargo);
+                }
+                aulasRelatorio.Add(aulaIndividual);
             }
+            return aulasRelatorio;
         }
 
-        private string RetornarDisciplinaDia(string dia, Aula aula, int periodo, string descricaoDisciplina)
+        private async Task<string> RetornarDisciplinaDia(string dia, Aula aula, CargoDisciplina disciplinaCargo)
         {
             if (!aula.Reserva.DiaSemana.Equals(dia))
                 return "";
 
-            return $"({periodo}° Período){Environment.NewLine}{descricaoDisciplina}";
+            var disciplinaCurriculo = await _curriculoDisciplinaRepositorio.Consultar(lnq => lnq.Codigo == disciplinaCargo.CodigoCurriculoDisciplina);
+
+            var disciplina = await ConsultarDisciplinaCurriculo(disciplinaCargo.CodigoCurriculoDisciplina);
+
+            return $"({disciplinaCurriculo.Periodo}° Período){Environment.NewLine}{disciplina.Descricao}";
         }
 
         private async Task<Disciplina> ConsultarDisciplinaCurriculo(int codigoCurriculoDisciplina)
