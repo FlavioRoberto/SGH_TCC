f80ae7c6d54c556ad884b0c0d8351a5473128b32
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-09-01 20:28:15 -0300

    Adicionando propriedade na view model da dsiciplina do cargo

--- Api/SGH/Dominio/Implementacao/CargosDisciplinas/ViewModel/CargoDisciplinaListarPorCurriculoViewModel.cs
+++ Api/SGH/Dominio/Implementacao/CargosDisciplinas/ViewModel/CargoDisciplinaListarPorCurriculoViewModel.cs
@@ -5,6 +5,7 @@ namespace SGH.Dominio.Services.Implementacao.CargosDisciplinas.ViewModel
     public class CargoDisciplinaListarPorCurriculoViewModel 
     {
         public int? Codigo { get; set; }
+        public long CodigoCurriculoDisciplina { get; set; }
         public string Descricao { get; set; }
         public string Professor { get; set; }
     }
